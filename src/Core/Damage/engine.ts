4840aae1f3b6e9422fc955305abb78d1bcc873d6
KrayOristine <trantrungho71@gmail.com>
2022-12-25 14:15:50 +0700

    finish damage engine

--- src/Core/Damage/engine.ts
+++ src/Core/Damage/engine.ts
@@ -13,11 +13,12 @@ import { LinkedList, Logger } from "wc3-treelib";
 const DEATH_VAL = 0.405; // Value which unit is determined as death
 const LIMBO_DEPTH = 8; // How much can the engine will be able to recursive
 export interface DamageInstance {
-	engineType: number | number[];
-	scriptType: number | number[];
-	gameType: number | number[];
-	userType: number | number[];
+	engineType: number[];
+	scriptType: number[];
+	gameType: number[];
+	userType: number[];
 	damage: number;
+	userAmt?: number;
 	source: Unit;
 	target: Unit;
 	owner: MapPlayer;
@@ -129,6 +130,102 @@ export class Damage {
 		this.t2 = new Trigger();
 		this.t3 = new Trigger();
 		this.targets = new Group();
+
+		this.t1.registerAnyUnitEvent(EVENT_PLAYER_UNIT_DAMAGING);
+		this.t1.addCondition(() => {
+			let d = this.createFromEvent();
+			if (this.alarmSet) {
+				if (this.totem) {
+					if (d.damageType == DAMAGE_TYPE_SPIRIT_LINK || d.damageType == DAMAGE_TYPE_DEFENSIVE || d.damageType == DAMAGE_TYPE_PLANT) {
+						this.lastInstance = this.current;
+						this.totem = false;
+						this.canKick = false;
+					} else this.failSafeClear();
+				} else this.finish();
+
+				if (d.source.handle != this.orgSource) {
+					this.onAOEEnd();
+					this.orgSource = d.source.handle;
+					this.orgTarget = d.target.handle;
+				} else if (d.target.handle == this.orgTarget) this.sourceStacks++;
+				else if (!this.targets.hasUnit(d.target)) this.sourceAOE++;
+			} else {
+				this.alarmSet = true;
+				this.alarm.start(0, false, () => {
+					this.alarmSet = false;
+					this.dreaming = false;
+					this.enable(true);
+					if (this.totem) this.failSafeClear();
+					else {
+						this.canKick = true;
+						this.kicking = false;
+						this.finish();
+					}
+					this.onAOEEnd();
+					this.current = null;
+				});
+				this.orgSource = d.source.handle;
+				this.orgTarget = d.target.handle;
+			}
+			this.targets.addUnit(d.target);
+			if (this.doPreEvents(d, true)) {
+				this.canKick = true;
+				this.finish();
+			}
+			this.totem =
+				!this.lastInstance ||
+				this.attackImmune[GetHandleId(d.attackType)] ||
+				this.damageImmune[GetHandleId(d.damageType)] ||
+				!d.target.isUnitType(UNIT_TYPE_MAGIC_IMMUNE);
+
+			return true; // Decor only, has nothing to do
+		});
+
+		this.t2.registerAnyUnitEvent(EVENT_PLAYER_UNIT_DAMAGED);
+		this.t2.addCondition(() => {
+			let r = GetEventDamage();
+			let d = this.current;
+			if (this.prepped) this.prepped = null;
+			else if (this.dreaming || d.prevAmt == 0) return;
+			else if (this.totem) this.totem = false;
+			else {
+				this.afterDamage();
+				d = this.lastInstance;
+				this.current = d;
+				this.lastInstance = null;
+				this.canKick = true;
+			}
+			this.setArmor(true);
+			d.userAmt = d.damage;
+			d.damage = r;
+
+			if (r > 0.0) {
+				this.runEvent("armor");
+				if (this.hasLethal) {
+					this.life = GetWidgetLife(d.target.handle) - d.damage;
+					if (this.life < DEATH_VAL) {
+						this.runEvent("lethal");
+						d.damage = GetWidgetLife(d.target.handle) - this.life;
+					}
+				}
+			}
+
+			if (d.damageType != DAMAGE_TYPE_UNKNOWN) this.runEvent("damaged");
+			BlzSetEventDamage(d.damage);
+			this.eventsRun = true;
+			if (d.damage == 0) this.finish();
+
+			return true; // Decor only, has nothing to do
+		});
+
+		this.t3.registerAnyUnitEvent(EVENT_PLAYER_UNIT_DAMAGING);
+		this.t3.addCondition(() => {
+			this.addRecursive(this.createFromEvent());
+			BlzSetEventDamage(0.0);
+
+			return true;
+		});
+		this.t3.enabled = false;
 	}
 	private static alarm: Timer;
 	private static alarmSet: boolean = false;
@@ -151,8 +248,43 @@ export class Damage {
 	private static orgTarget: unit;
 	public static next: DamageInstance; // The next value to apply if exist
 	private static targets: Group;
-	private static attackImmune: boolean[] = [false, true, true, true, false, true, true];
-	private static damageImmune: boolean[] = [];
+	private static attackImmune: boolean[] = [
+		false, // ATTACK_TYPE_NORMAL
+		true, // ATTACK_TYPE_MELEE
+		true, // ATTACK_TYPE_PIERCE
+		true, // ATTACK_TYPE_SIEGE
+		false, // ATTACK_TYPE_MAGIC
+		true, // ATTACK_TYPE_CHAOS
+		true, // ATTACK_TYPE_HERO
+	];
+	private static damageImmune: boolean[] = [
+		true, // DAMAGE_TYPE_UNKNOWN
+		false, // NONE
+		false, // NONE
+		false, // NONE
+		true, // DAMAGE_TYPE_NORMAL
+		true, // DAMAGE_TYPE_ENHANCED
+		false, // NONE
+		false, // DAMAGE_TYPE_FIRE
+		false, // DAMAGE_TYPE_COLD
+		false, // DAMAGE_TYPE_LIGHTNING
+		true, // DAMAGE_TYPE_POISON
+		true, // DAMAGE_TYPE_DISEASE
+		false, // DAMAGE_TYPE_DIVINE
+		false, // DAMAGE_TYPE_MAGIC
+		false, // DAMAGE_TYPE_SONIC
+		true, // DAMAGE_TYPE_ACID
+		false, // DAMAGE_TYPE_FORCE
+		false, // DAMAGE_TYPE_DEATH
+		false, // DAMAGE_TYPE_MIND
+		false, // DAMAGE_TYPE_PLANT
+		false, // DAMAGE_TYPE_DEFENSIVE
+		true, // DAMAGE_TYPE_DEMOLITION
+		true, // DAMAGE_TYPE_SLOW_POISON
+		false, // DAMAGE_TYPE_SPIRIT_LINK
+		false, // DAMAGE_TYPE_SHADOW_STRIKE
+		true, // DAMAGE_TYPE_UNIVERSAL
+	];
 	private static recursiveStacks: DamageInstance[] = [];
 	private static override: boolean = false;
 	private static life: number = 0;
@@ -239,7 +371,7 @@ export class Damage {
 
 	private static breakCheck = {
 		hit: () => false,
-		damage: () => Damage.current.engineType == 2,
+		damage: () => this.override || Damage.current.engineType[0] == 2,
 		armor: () => Damage.current.damage <= 0.0,
 		damaged: () => damagedOrAfter,
 		after: () => damagedOrAfter,
@@ -305,10 +437,10 @@ export class Damage {
 			attackType: attackType,
 			damageType: damageType,
 			weaponType: weaponType,
-			userType: 0,
-			engineType: 0,
-			scriptType: 0,
-			gameType: 0,
+			userType: [],
+			engineType: [0],
+			scriptType: [0],
+			gameType: [0],
 			isMelee: IsUnitType(source, UNIT_TYPE_MELEE_ATTACKER) && !isRanged,
 			isSpell: attackType == ATTACK_TYPE_NORMAL && damageType != DAMAGE_TYPE_NORMAL,
 			prevAmt: amt,
@@ -319,10 +451,10 @@ export class Damage {
 		};
 
 		if (this.next) {
-			d.userType = this.next.userType || 0;
-			d.engineType = this.next.engineType || 0;
-			d.scriptType = this.next.scriptType || 0;
-			d.gameType = this.next.gameType || 0;
+			d.userType = this.next.userType || d.userType;
+			d.engineType = this.next.engineType || d.engineType;
+			d.scriptType = this.next.scriptType || d.scriptType;
+			d.gameType = this.next.gameType || d.gameType;
 			d.attackType = this.next.attackType || d.attackType;
 			d.damageType = this.next.damageType || d.damageType;
 			d.isSpell = this.next.isSpell || d.isSpell;
@@ -468,5 +600,28 @@ export class Damage {
 		this.enable(true);
 		this.recursiveSource = new WeakMap<unit, boolean>();
 		this.recursiveTarget = new WeakMap<unit, boolean>();
+	}
+
+	private static failSafeClear() {
+		this.setArmor(true);
+		this.canKick = true;
+		this.kicking = false;
+		this.totem = false;
+		this.runEvent("damaged");
+		this.eventsRun = true;
+		this.finish();
+	}
+
+	private static createFromEvent() {
+		return this.create(
+			GetEventDamageSource(),
+			GetTriggerUnit(),
+			GetEventDamage(),
+			BlzGetEventIsAttack(),
+			false,
+			BlzGetEventAttackType(),
+			BlzGetEventDamageType(),
+			BlzGetEventWeaponType()
+		);
 	}
 }
